5bc3c7d255817326c2e0571f9cd1c0406244845a
suzana <suzana_07@hotmail.com>
2024-03-27 15:49:19 -0300

    medindo

---
+++ conversor.js
@@ -0,0 +1,8 @@
+const medida = prompt("Insira uma  medida em metros")
+
+const unidade = prompt ("Para qual unidade de medida deseja converter?" +  "\n1 - milímetros (mm)" +
+"\n2 - centímetros (cm)" +
+"\n3 - decímetros (dm)" +
+"\n4 - decâmetros (dam)" +
+"\n5 - hectômetro (hm)" +
+"\n6 - quilômetro (km)")
